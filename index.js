04f8a7dba271979094dcd796d7ece9cc8d9d533b
Jason Yu <ycm.jason@gmail.com>
2017-02-24 12:02:10 +0000

    refactor

--- index.js
+++ index.js
@@ -10,21 +10,19 @@ var PORT = process.env.PORT || 8080;
 
 var DEFAULT_CONFIG_PATH = homedir() + '/.bouncy-forever.config.json'
 var config_path = process.argv[2]? process.cwd() + '/' + process.argv[2]: DEFAULT_CONFIG_PATH;
-var config = config(config_path);
-
-var domain_port_map = {};
+var config = config(config_path, PORT);
 
 // countdown latch to forcefully make sure all sub-servers are launched before the bouncy server does.
 var latch = new Latch(config.getDomains().length, launchBouncyServer);
 
 // launch each sub-servers
-config.each((domain, app_config) => {
+config.each((domain, app) => {
   freeport((err, port) => {
     if(err) throw err;
 
-    subprocess(app_config).start(port, function(){
-      domain_port_map[domain] = port;
-      console.log(`${app_config.name} launched and listening on port ${port}...`);
+    subprocess(app).start(port, function(){
+      console.log(`${app.name} launched and listening on port ${port}...`);
+      app.port = port;
       latch.down();
     });
   });
@@ -32,20 +30,12 @@ config.each((domain, app_config) => {
 
 function launchBouncyServer(){
   console.log();
-  // append port to the domains if PORT != 80
-  if(PORT != 80){
-    var temp = domain_port_map;
-    domain_port_map = {};
-    Object.keys(temp).forEach(domain => {
-      domain_port_map[domain + ':' + PORT] = temp[domain];
-    });
-  }
 
-  var server = proxy(domain_port_map);
+  var server = proxy(config.getDomainPortMap());
   server.listen(PORT, function(){
     console.log(`Mapping...`);
-    Object.keys(domain_port_map).forEach(domain => {
-      console.log(`  ${domain} to port ${domain_port_map[domain]}`);
+    config.each((domain, app) => {
+      console.log(`  \`${app.name}\` launched and listening to port ${app.port}`);
     });
     console.log(`bouncy-forever server launched and listening on port ${PORT}...`);
   });
